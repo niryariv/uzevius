9a213268ebaf65516763d8d6847bc27eab9ebbd2
Nir Yariv <niryariv@gmail.com>
2021-07-02 12:02:18 +0300

    with video modals

--- code.js
+++ code.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    video_modal = new bootstrap.Modal(document.getElementById('video_modal'))
+    // stop video when the modal is closed
+    $('#video_modal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src', '');
+    });
+
+
     maplibregl.setRTLTextPlugin(
         'https://cdn.maptiler.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.2/mapbox-gl-rtl-text.js',
         null,
@@ -41,17 +49,45 @@ $(document).ready(function () {
         })
 
         function render_point(p){
-            lnglat = p.geometry.coordinates;
-            rotation = p.properties.rotation || 0;
-            color = p.properties.color || '#bbdaf9';
-            title = p.properties.title;
+            console.log(p);
+            var lnglat = p.geometry.coordinates;
+            var rotation = p.properties.rotation || 0;
+            var color = p.properties.color || '#bbdaf9';
+            var title = p.properties.title;
+            var video_src = 'https://player.vimeo.com/video/'+ p.properties.vimeo +'?autoplay=1&title=0&byline=0&portrait=0'; //'https://player.vimeo.com/video/5922384?title=0&byline=0&portrait=0';
 
-            console.log()
+            // var markerHeight = 50, markerRadius = 10, linearOffset = 25;
+            // var popupOffsets = {
+            //     'top': [-100, -100],
+            //     'top-left': [-100, -100],
+            //     'top-right': [0, 0],
+            //     'bottom': [0, -markerHeight],
+            //     'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+            //     'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+            //     'left': [markerRadius, (markerHeight - markerRadius) * -1],
+            //     'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+            // };
 
             var marker = new maplibregl.Marker({ color: color, rotation: rotation })
                 .setLngLat(lnglat)
-                .setPopup(new maplibregl.Popup({ offset: 25, closeButton: false }).setText(title))
-                .addTo(map);
+                .setPopup(
+                    new maplibregl.Popup({ 
+                        offset: 25, 
+                        closeButton: false
+                    })
+                    .setMaxWidth('fit-content')
+                    .setHTML(title)
+                    .on('open', function(currentFeature){
+                        console.log(currentFeature)
+                        map.flyTo({
+                            center: currentFeature.target.getLngLat(),
+                            zoom: 15
+                        });
+                        $('#video').attr('src', video_src);
+                        $("#video_title").html(title)
+                        video_modal.show()
+                    })
+                ).addTo(map);
         }
 
 
@@ -74,16 +110,16 @@ $(document).ready(function () {
 
     });
 
-    map.on('click', function (e) {
-        document.getElementById('info').innerHTML =
-            // e.point is the x, y coordinates of the mousemove event relative
-            // to the top-left corner of the map
-            JSON.stringify(e.point) +
-            '<br />' +
-            // e.lngLat is the longitude, latitude geographical position of the event
-            JSON.stringify(e.lngLat.wrap());
-            console.log(e.lngLat);
-    });
+    // map.on('click', function (e) {
+    //     document.getElementById('info').innerHTML =
+    //         // e.point is the x, y coordinates of the mousemove event relative
+    //         // to the top-left corner of the map
+    //         JSON.stringify(e.point) +
+    //         '<br />' +
+    //         // e.lngLat is the longitude, latitude geographical position of the event
+    //         JSON.stringify(e.lngLat.wrap());
+    //         console.log(e.lngLat);
+    // });
 
     // map.flyTo({
     //     center: [35.358, 31.316],
