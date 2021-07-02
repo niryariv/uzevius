6c410b3e5d8d49987ec47e13db7cbb4384b083c9
Nir Yariv <niryariv@gmail.com>
2021-07-02 15:28:49 +0300

    add service workers

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', async event => {
+    console.log('SW install');
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', async event => {
+    console.log('SW fetch');
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || fetch(req);
+}
+
+const cacheName = 'uzevius-v1';
+const staticAssets = [
+    './',
+    './index.html',
+    './code.js',
+    './style.css',
+    './data/points.geojson'
+];
