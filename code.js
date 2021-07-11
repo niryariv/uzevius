ec9c3c5103577624f477488ada2286c6e7dd2f6a
Nir Yariv <niryariv@gmail.com>
2021-07-11 11:05:07 +0300

    cleanup

--- code.js
+++ code.js
@@ -1,16 +1,4 @@
-var Markers = {};
-
 $(document).ready(function () {
-
-    // Sentry.init({
-    //     dsn: "https://eec507480d9449bd8a33698b709dcb35@o914112.ingest.sentry.io/5852590",
-    //     integrations: [new Integrations.BrowserTracing()],
-    //     tracesSampleRate: 1.0,
-    // });
-
-    // Sentry.captureMessage("Sentry On");
-
-
 
     maplibregl.setRTLTextPlugin(
         'https://cdn.maptiler.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.2/mapbox-gl-rtl-text.js',
@@ -21,11 +9,10 @@ $(document).ready(function () {
     var map = new maplibregl.Map({
         container: 'map',
         style: 'https://api.maptiler.com/maps/topo/style.json?key=cgzcpq242p8x5zNNGxpx',
-        // center: [0,0],
-        zoom: 7,
+        zoom: 10,
         // center: [34.102, 30.935],
-        customAttribution: "v11.7.0"
-        // maxBounds: [[35.33, 31.3], [35.38, 31.34]],
+        customAttribution: "v11.7.1",
+        maxBounds: [[34, 29], [36, 33]]
     });
 
 
@@ -63,7 +50,6 @@ $(document).ready(function () {
             console.log(e);
         }).done(function(d){
             console.log(d);
-            debugger;
             map.flyTo({
                 center: d.features[0].geometry.coordinates,
                 zoom: 15
@@ -74,13 +60,13 @@ $(document).ready(function () {
         })
 
         function render_point(p){
-            console.log(p);
+            // console.log(p);
             var lnglat = p.geometry.coordinates;
             var rotation = 0; // ignore rotation for images p.properties.rotation || 0;
             var color = p.properties.color || '#bbdaf9';
             var title = p.properties.title;
             var video_src = p.properties.youtube;
-                // 'https://player.vimeo.com/video/'+ p.properties.vimeo +'?autoplay=1&title=0&byline=0&portrait=0'; //'https://player.vimeo.com/video/5922384?title=0&byline=0&portrait=0';
+            // 'https://player.vimeo.com/video/'+ p.properties.vimeo +'?autoplay=1&title=0&byline=0&portrait=0'; //'https://player.vimeo.com/video/5922384?title=0&byline=0&portrait=0';
 
             el = document.createElement('div');
             el.className = 'image_marker';
